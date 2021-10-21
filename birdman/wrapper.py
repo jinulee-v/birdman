c5aca1fd9e313d6ae1675349f9eb75c4545d34e5
jinulee-v <aquamrn@snu.ac.kr>
2021-10-21 05:30:02 +0000

    birdman streamer working test; listener requires test

--- birdman/wrapper.py
+++ birdman/wrapper.py
@@ -14,11 +14,18 @@ class Birdman(object):
     def __init__(self, streamers, listeners):
         self._streamers = streamers
         self._listeners = listeners
+        
+        for streamer in streamers:
+            if not isinstance(streamer, BaseStreamer):
+                raise ValueError("`streamers` argument must be an iterable of BaseStreamer objects")
+        for listener in listeners:
+            if not isinstance(listener, BaseListener):
+                raise ValueError("`listeners` argument must be an iterable of BaseListener objects")
 
     async def _stream_routine(self):
         """Asynchronous streaming & listening starts here.
         """
-        self._stream = aiostream.stream.merge([
+        self._stream = aiostream.stream.merge(*[
             streamer.stream() for streamer in self._streamers
         ])
         async with self._stream.stream() as streamer:
@@ -32,13 +39,13 @@ class Birdman(object):
         self.loop = asyncio.get_event_loop()
 
         try:
-            self.loop.run_forever(self._stream_routine())
+            self.loop.run_until_complete(self._stream_routine())
         finally:
             self.loop.close()
 
     def add_streamer(self, streamer):
         if not isinstance(streamer, BaseStreamer):
-            raise ValueError("Streamer object must inherit from BaseStreamer")
+            raise ValueError("`streamer` argument must be a BaseStreamer object")
 
         self.loop.stop()
         self._streamers.append(streamer)
@@ -46,8 +53,24 @@ class Birdman(object):
 
     def add_listener(self, listener):
         if not isinstance(listener, BaseListener):
-            raise ValueError("Listener object must inherit from BaseListener")
+            raise ValueError("`listener` argument must be a BaseListener object")
 
         self.loop.stop()
         self._listeners.append(listener)
         self.start()
+
+
+def main():
+    from birdman.stream.dcinside import DCInsideStreamer
+    from birdman.stream.todayhumor import TodayHumorStreamer
+
+    streamers = [
+        DCInsideStreamer({'verbose': 1}),
+        TodayHumorStreamer({'verbose': 1, 'include_comments': 0})
+    ]
+    birdman = Birdman(streamers, [])
+    birdman.start()
+
+
+if __name__ == '__main__':
+    main()
